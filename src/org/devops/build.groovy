a4c2fbf880a619161f15c93427dfdeaed9da1bf8
fudengrong <35551455+fudengrong@users.noreply.github.com>
2022-06-12 16:49:51 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -7,7 +7,7 @@ def Build(BuildType,BuildShell){
     println("当前选中构建类型: ${BuildType}")
     BuildHome = tool BuildTools[BuildType]
     
-    if ("${BuildType}"= "node"){
+    if ("${BuildType}"== "node"){
         sh """
            export npmHome=/usr/share/nginx/html/node-v16.15.1-linux-x64
            export PATH=\$PATH:\$npmHome/bin
